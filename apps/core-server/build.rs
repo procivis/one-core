b7e1d2befca1fdd4d65a90a2ced9e04bd7f19f2e
Mateusz Szychowski <szychowski@procivis.ch>
2023-06-30 15:34:55 +0200

    ONE-394 Use APP_VERSION variable from CI in swagger-ui

--- apps/core-server/build.rs
+++ apps/core-server/build.rs
@@ -6,11 +6,23 @@ fn main() -> SdResult<()> {
     shadow_rs::new_hook(hook)
 }
 
+fn get_app_version_string() -> String {
+    let value = envmnt::get_or("APP_VERSION", "");
+    if value.is_empty() {
+        "None".to_string()
+    } else {
+        format!(r#"Some("{value}")"#)
+    }
+}
+
 fn hook(mut file: &File) -> SdResult<()> {
     // Here we need to extract and put variables to the file one by one.
+    let app_version = get_app_version_string();
     let ci_pipeline_id = envmnt::get_or("CI_PIPELINE_ID", "NOT PROVIDED");
-    let hook_const: &str =
-        &format!("#[allow(dead_code)] pub const CI_PIPELINE_ID: &str = \"{ci_pipeline_id}\";");
+    let hook_const: &str = &format!(
+        r#"#[allow(dead_code)] pub const APP_VERSION: Option<&str> = {app_version};
+           #[allow(dead_code)] pub const CI_PIPELINE_ID: &str = "{ci_pipeline_id}";"#
+    );
     writeln!(file, "{hook_const}")?;
     Ok(())
 }
