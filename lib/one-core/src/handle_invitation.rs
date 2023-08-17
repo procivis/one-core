e6043153e826a23bf1ed93224b0dc6412c14e36b
Mateusz Szychowski <szychowski@procivis.ch>
2023-08-17 12:00:07 +0200

    ONE-508 Add Transport to Config

--- lib/one-core/src/handle_invitation.rs
+++ lib/one-core/src/handle_invitation.rs
@@ -412,6 +412,7 @@ mod tests {
         let minimal_config = r#"{
           "format": {},
           "exchange": {},
+          "transport": {},
           "revocation": {},
           "did": {},
           "datatype": {}
