4d558e1f7426feb2cf90cf5af167b7862c4fb9c9
Developer <info@procivis.ch>
2023-08-28 10:44:08 +0200

    * did repository unit-tests

--- lib/sql-data-provider/src/did/mod.rs
+++ lib/sql-data-provider/src/did/mod.rs
@@ -3,8 +3,13 @@ use sea_orm::DatabaseConnection;
 pub mod mapper;
 pub mod repository;
 
+mod create_did;
+mod get_did;
+mod get_did_by_value;
+mod get_did_list;
+
 #[cfg(test)]
-mod test;
+mod test_utilities;
 
 pub(crate) struct DidProvider {
     pub db: DatabaseConnection,
