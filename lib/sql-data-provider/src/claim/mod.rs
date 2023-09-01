b85c4016af33083f5382c1f8d711ea625e045dcb
Developer <info@procivis.ch>
2023-09-01 16:11:25 +0200

    ONE-685: Claim repository

---
+++ lib/sql-data-provider/src/claim/mod.rs
@@ -0,0 +1,11 @@
+use one_core::repository::claim_schema_repository::ClaimSchemaRepository;
+use sea_orm::DatabaseConnection;
+use std::sync::Arc;
+
+pub mod mapper;
+pub mod repository;
+
+pub(crate) struct ClaimProvider {
+    pub db: DatabaseConnection,
+    pub claim_schema_repository: Arc<dyn ClaimSchemaRepository + Send + Sync>,
+}
