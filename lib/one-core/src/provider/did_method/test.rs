481250322fd3cad97b93aa35f02fad58cebac42f
Developer <info@procivis.ch>
2023-10-20 14:36:58 +0200

    ONE-785 tests for CreateDid

--- lib/one-core/src/provider/did_method/test.rs
+++ lib/one-core/src/provider/did_method/test.rs
@@ -2,8 +2,13 @@ use super::key::KeyDidMethod;
 use super::provider::DidMethodProvider;
 use crate::config::data_structure::DidKeyParams;
 use crate::model::did::{Did, DidType};
+use crate::model::key::Key;
+use crate::provider::did_method::DidMethodError;
+use crate::provider::key_storage::mock_key_storage::MockKeyStorage;
 use crate::provider::key_storage::provider::KeyProviderImpl;
+use crate::provider::key_storage::KeyStorage;
 use crate::repository::mock::organisation_repository::MockOrganisationRepository;
+use crate::service::did::dto::{CreateDidRequestDTO, CreateDidRequestKeysDTO};
 use crate::{
     model::{did::KeyRole, organisation::Organisation},
     provider::did_method::{provider::DidMethodProviderImpl, DidMethod},
@@ -13,16 +18,25 @@ use std::{collections::HashMap, sync::Arc};
 use time::OffsetDateTime;
 use uuid::Uuid;
 
-fn setup_provider(did_repository: MockDidRepository) -> Arc<dyn DidMethodProvider + Send + Sync> {
+fn setup_provider(
+    did_repository: MockDidRepository,
+    key_storage: MockKeyStorage,
+    organisation_repository: MockOrganisationRepository,
+) -> Arc<dyn DidMethodProvider + Send + Sync> {
     let did_repository = Arc::new(did_repository);
+
+    let mut key_storages: HashMap<String, Arc<dyn KeyStorage + Send + Sync>> = HashMap::new();
+    key_storages.insert("MOCK".to_string(), Arc::new(key_storage));
+
+    let key_provider = KeyProviderImpl::new(key_storages);
 
     let mut did_methods: HashMap<String, Arc<dyn DidMethod + Send + Sync>> = HashMap::new();
     did_methods.insert(
         "KEY".to_string(),
         Arc::new(KeyDidMethod {
             did_repository: did_repository.clone(),
-            organisation_repository: Arc::new(MockOrganisationRepository::default()),
-            key_provider: Arc::new(KeyProviderImpl::new(HashMap::new())),
+            organisation_repository: Arc::new(organisation_repository),
+            key_provider: Arc::new(key_provider),
             method_key: "KEY".to_string(),
             params: DidKeyParams::default(),
         }),
@@ -65,7 +79,11 @@ async fn test_did_key_resolve() {
                 organisation: None,
             })
         });
-    let provider = setup_provider(did_repository);
+    let provider = setup_provider(
+        did_repository,
+        MockKeyStorage::default(),
+        MockOrganisationRepository::default(),
+    );
 
     let organisation = Organisation {
         id: Uuid::new_v4(),
@@ -87,4 +105,141 @@ async fn test_did_key_resolve() {
             .key;
         assert_eq!(bs58::encode(key.public_key).into_string(), public_key);
     }
+}
+
+fn generic_did() -> Did {
+    Did {
+        id: Uuid::new_v4(),
+        created_date: OffsetDateTime::now_utc(),
+        last_modified: OffsetDateTime::now_utc(),
+        name: "".to_string(),
+        did: "did:key:MOCK".to_string(),
+        did_type: DidType::Local,
+        did_method: "KEY".to_string(),
+        keys: None,
+        organisation: None,
+    }
+}
+
+#[tokio::test]
+async fn test_create_did_success() {
+    let key_id = Uuid::new_v4();
+
+    let create_request = CreateDidRequestDTO {
+        name: "name".to_string(),
+        organisation_id: Uuid::new_v4(),
+        did_type: DidType::Local,
+        did_method: "KEY".to_string(),
+        keys: CreateDidRequestKeysDTO {
+            authentication: vec![key_id],
+            assertion: vec![],
+            key_agreement: vec![],
+            capability_invocation: vec![],
+            capability_delegation: vec![],
+        },
+        params: None,
+    };
+    let key = Key {
+        id: key_id,
+        created_date: OffsetDateTime::now_utc(),
+        last_modified: OffsetDateTime::now_utc(),
+        public_key: vec![],
+        name: "".to_string(),
+        private_key: vec![],
+        storage_type: "MOCK".to_string(),
+        key_type: "EDDSA".to_string(),
+        organisation: None,
+    };
+
+    let mut key_storage = MockKeyStorage::default();
+    key_storage
+        .expect_fingerprint()
+        .times(1)
+        .returning(move |_| "did:key:MOCK".to_string());
+
+    let mut did_repository = MockDidRepository::default();
+    did_repository
+        .expect_get_did_by_value()
+        .times(1)
+        .returning(|_, _| Err(crate::repository::error::DataLayerError::RecordNotFound));
+
+    let did = generic_did();
+    did_repository
+        .expect_create_did()
+        .times(1)
+        .returning(move |_| Ok(did.id));
+    did_repository
+        .expect_get_did()
+        .times(1)
+        .returning(move |_, _| Ok(did.clone()));
+
+    let mut organisation_repository = MockOrganisationRepository::default();
+    organisation_repository
+        .expect_get_organisation()
+        .times(1)
+        .returning(|id, _| {
+            Ok(Organisation {
+                id: id.to_owned(),
+                created_date: OffsetDateTime::now_utc(),
+                last_modified: OffsetDateTime::now_utc(),
+            })
+        });
+
+    let provider = setup_provider(did_repository, key_storage, organisation_repository);
+    let did_method = provider.get_did_method("KEY").unwrap();
+    let result = did_method.create(create_request, key).await;
+    result.unwrap();
+}
+
+#[tokio::test]
+async fn test_create_did_already_exists() {
+    let key_id = Uuid::new_v4();
+
+    let create_request = CreateDidRequestDTO {
+        name: "name".to_string(),
+        organisation_id: Uuid::new_v4(),
+        did_type: DidType::Local,
+        did_method: "KEY".to_string(),
+        keys: CreateDidRequestKeysDTO {
+            authentication: vec![key_id],
+            assertion: vec![],
+            key_agreement: vec![],
+            capability_invocation: vec![],
+            capability_delegation: vec![],
+        },
+        params: None,
+    };
+    let key = Key {
+        id: key_id,
+        created_date: OffsetDateTime::now_utc(),
+        last_modified: OffsetDateTime::now_utc(),
+        public_key: vec![],
+        name: "".to_string(),
+        private_key: vec![],
+        storage_type: "MOCK".to_string(),
+        key_type: "EDDSA".to_string(),
+        organisation: None,
+    };
+
+    let mut key_storage = MockKeyStorage::default();
+    key_storage
+        .expect_fingerprint()
+        .times(1)
+        .returning(move |_| "did:key:MOCK".to_string());
+
+    let mut did_repository = MockDidRepository::default();
+    did_repository
+        .expect_get_did_by_value()
+        .times(1)
+        .returning(|_, _| Ok(generic_did()));
+
+    let provider = setup_provider(
+        did_repository,
+        key_storage,
+        MockOrganisationRepository::default(),
+    );
+    let did_method = provider.get_did_method("KEY").unwrap();
+    let result = did_method.create(create_request, key).await;
+    assert!(result.is_err());
+    assert!(matches!(result, Err(DidMethodError::AlreadyExists)));
 }
