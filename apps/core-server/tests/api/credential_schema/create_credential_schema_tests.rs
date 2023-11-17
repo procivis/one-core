262f38a4cba471dd4a0c89d7b410175791d0fdb6
Developer <info@procivis.ch>
2023-11-17 18:20:02 +0100

    ONE-1112: Integration tests for credential schema

---
+++ apps/core-server/tests/api/credential_schema/create_credential_schema_tests.rs
@@ -0,0 +1,48 @@
+use crate::{fixtures, utils};
+use core_server::router::start_server;
+use httpmock::MockServer;
+use serde_json::{json, Value};
+
+#[tokio::test]
+async fn test_create_credential_schema_success() {
+    // GIVEN
+    let mock_server = MockServer::start_async().await;
+    let config = fixtures::create_config(mock_server.base_url());
+    let db_conn = fixtures::create_db(&config).await;
+    let organisation_id = fixtures::create_organisation(&db_conn).await;
+
+    // WHEN
+    let listener = std::net::TcpListener::bind("127.0.0.1:0").unwrap();
+    let base_url = format!("http://{}", listener.local_addr().unwrap());
+
+    let url = format!("{base_url}/api/credential-schema/v1");
+
+    let _handle = tokio::spawn(async move { start_server(listener, config, db_conn).await });
+
+    let resp = utils::client()
+        .post(url)
+        .bearer_auth("test")
+        .json(&json!({
+          "claims": [
+            {
+              "datatype": "STRING",
+              "key": "firstName",
+              "required": true
+            }
+          ],
+          "format": "JWT",
+          "name": "some credential schema",
+          "organisationId": organisation_id,
+          "revocationMethod": "STATUSLIST2021"
+        }))
+        .send()
+        .await
+        .unwrap();
+
+    // THEN
+    assert_eq!(resp.status(), 201);
+    let resp: Value = resp.json().await.unwrap();
+
+    assert!(resp.get("id").is_some());
+    // TODO: Add additional checks when https://procivis.atlassian.net/browse/ONE-1133 is implemented
+}
--- apps/core-server/tests/integration_tests.rs
+++ apps/core-server/tests/integration_tests.rs
@@ -1,6 +1,15 @@
 mod fixtures;
 mod utils;
 
+// CREDENTIAL SCHEMA
+#[path = "api/credential_schema/create_credential_schema_tests.rs"]
+mod create_credential_schema_tests;
+#[path = "api/credential_schema/delete_credential_schema_tests.rs"]
+mod delete_credential_schema_tests;
+#[path = "api/credential_schema/get_credential_schema_tests.rs"]
+mod get_credential_schema_tests;
+#[path = "api/credential_schema/list_credential_schema_tests.rs"]
+mod list_credential_schema_tests;
 // CREDENTIAL
 #[path = "api/credential/create_credential_tests.rs"]
 mod create_credential_tests;
@@ -14,11 +23,13 @@ mod revoke_credential_tests;
 #[path = "api/credential/share_credential_tests.rs"]
 mod share_credential_tests;
 
+// OIDC
+#[path = "api/oidc/direct_post_tests.rs"]
+mod direct_post_tests;
+
 // PROOF
 #[path = "api/proof/create_proof_tests.rs"]
 mod create_proof_tests;
-#[path = "api/oidc/direct_post_tests.rs"]
-mod direct_post_tests;
 #[path = "api/proof/get_presentation_definition_tests.rs"]
 mod get_presentation_definition_tests;
 
