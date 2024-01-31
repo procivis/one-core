7d10bfc7f81678fe83fe7a60a076f5b99b3b9756
Developer <info@procivis.ch>
2024-01-31 16:09:08 +0100

    ONE-1505: Protect from fetching invalid entity type

--- lib/sql-data-provider/src/history/test.rs
+++ lib/sql-data-provider/src/history/test.rs
@@ -12,6 +12,7 @@ use one_core::{
     },
     repository::history_repository::HistoryRepository,
 };
+use shared_types::DidId;
 
 use crate::{history::HistoryProvider, test_utilities::*};
 
@@ -35,6 +36,160 @@ async fn setup_empty() -> TestSetup {
             last_modified: get_dummy_date(),
         },
         db,
+    }
+}
+
+struct TestSetupWithCredentialsSchemaAndProof {
+    pub provider: HistoryProvider,
+    pub organisation: Organisation,
+    pub credential_schema_id: String,
+    pub did_id: DidId,
+    pub credential_id: String,
+}
+
+async fn setup_with_credential_schema_and_proof() -> TestSetupWithCredentialsSchemaAndProof {
+    let TestSetup {
+        provider,
+        organisation,
+        db,
+        ..
+    } = setup_empty().await;
+
+    insert_history(
+        &db,
+        HistoryAction::Created.into(),
+        organisation.id.to_owned().into(),
+        HistoryEntityType::Organisation.into(),
+        organisation.id.to_owned().into(),
+    )
+    .await
+    .unwrap();
+
+    let credential_schema_id = insert_credential_schema_to_database(
+        &db,
+        None,
+        &organisation.id.to_string(),
+        "schema",
+        "JWT",
+        "NONE",
+    )
+    .await
+    .unwrap();
+    insert_history(
+        &db,
+        HistoryAction::Created.into(),
+        Uuid::parse_str(&credential_schema_id).unwrap().into(),
+        HistoryEntityType::CredentialSchema.into(),
+        organisation.id.to_owned().into(),
+    )
+    .await
+    .unwrap();
+
+    let did_id = insert_did_key(
+        &db,
+        "issuer",
+        Uuid::new_v4(),
+        "did:key:123".parse().unwrap(),
+        "KEY",
+        &organisation.id.to_string(),
+    )
+    .await
+    .unwrap();
+    insert_history(
+        &db,
+        HistoryAction::Created.into(),
+        did_id.into(),
+        HistoryEntityType::Did.into(),
+        organisation.id.to_owned().into(),
+    )
+    .await
+    .unwrap();
+
+    let credential_id = insert_credential(
+        &db,
+        &credential_schema_id,
+        CredentialStateEnum::Created,
+        "PROCIVIS_TEMPORARY",
+        did_id.to_owned(),
+        None,
+    )
+    .await
+    .unwrap();
+    insert_history(
+        &db,
+        HistoryAction::Issued.into(),
+        Uuid::parse_str(&credential_id).unwrap().into(),
+        HistoryEntityType::Credential.into(),
+        organisation.id.to_owned().into(),
+    )
+    .await
+    .unwrap();
+
+    let claim_schema: Vec<(Uuid, &str, bool, u32, &str)> =
+        vec![(Uuid::new_v4(), "test", false, 0, "STRING")];
+    insert_many_claims_schema_to_database(&db, &credential_schema_id.to_string(), &claim_schema)
+        .await
+        .unwrap();
+
+    let claims: Vec<(Uuid, Uuid, &str, Vec<u8>)> = vec![(
+        Uuid::new_v4(),
+        claim_schema[0].0.to_owned(),
+        &credential_id,
+        vec![],
+    )];
+    insert_many_claims_to_database(&db, claims.as_slice())
+        .await
+        .unwrap();
+
+    let proof_schema_id = Uuid::parse_str(
+        &insert_proof_schema_with_claims_to_database(
+            &db,
+            None,
+            &claim_schema,
+            &organisation.id.to_string(),
+            "proof schema",
+        )
+        .await
+        .unwrap(),
+    )
+    .unwrap();
+    insert_history(
+        &db,
+        HistoryAction::Created.into(),
+        proof_schema_id.into(),
+        HistoryEntityType::ProofSchema.into(),
+        organisation.id.to_owned().into(),
+    )
+    .await
+    .unwrap();
+
+    let proof_id = Uuid::parse_str(
+        &insert_proof_request_to_database(&db, did_id, None, &proof_schema_id.to_string(), None)
+            .await
+            .unwrap(),
+    )
+    .unwrap();
+    let proof_claim: Vec<(Uuid, Uuid)> = vec![(proof_id.to_owned(), claims[0].0.to_owned())];
+    insert_many_proof_claim_to_database(&db, proof_claim.as_slice())
+        .await
+        .unwrap();
+
+    insert_history(
+        &db,
+        HistoryAction::Created.into(),
+        proof_id.into(),
+        HistoryEntityType::Proof.into(),
+        organisation.id.to_owned().into(),
+    )
+    .await
+    .unwrap();
+
+    TestSetupWithCredentialsSchemaAndProof {
+        provider,
+        organisation,
+        credential_schema_id,
+        did_id,
+        credential_id,
     }
 }
 
@@ -212,141 +367,13 @@ async fn test_get_history_list_schema_joins_credentials() {
 
 #[tokio::test]
 async fn test_get_history_list_joins_schema_credential_claim_and_proof() {
-    let TestSetup {
+    let TestSetupWithCredentialsSchemaAndProof {
         provider,
         organisation,
-        db,
+        credential_id,
+        credential_schema_id,
         ..
-    } = setup_empty().await;
-
-    insert_history(
-        &db,
-        HistoryAction::Created.into(),
-        organisation.id.to_owned().into(),
-        HistoryEntityType::Organisation.into(),
-        organisation.id.to_owned().into(),
-    )
-    .await
-    .unwrap();
-
-    let credential_schema_id = insert_credential_schema_to_database(
-        &db,
-        None,
-        &organisation.id.to_string(),
-        "schema",
-        "JWT",
-        "NONE",
-    )
-    .await
-    .unwrap();
-    insert_history(
-        &db,
-        HistoryAction::Created.into(),
-        Uuid::parse_str(&credential_schema_id).unwrap().into(),
-        HistoryEntityType::CredentialSchema.into(),
-        organisation.id.to_owned().into(),
-    )
-    .await
-    .unwrap();
-
-    let did_id = insert_did_key(
-        &db,
-        "issuer",
-        Uuid::new_v4(),
-        "did:key:123".parse().unwrap(),
-        "KEY",
-        &organisation.id.to_string(),
-    )
-    .await
-    .unwrap();
-    insert_history(
-        &db,
-        HistoryAction::Created.into(),
-        did_id.into(),
-        HistoryEntityType::Did.into(),
-        organisation.id.to_owned().into(),
-    )
-    .await
-    .unwrap();
-
-    let credential_id = insert_credential(
-        &db,
-        &credential_schema_id,
-        CredentialStateEnum::Created,
-        "PROCIVIS_TEMPORARY",
-        did_id.to_owned(),
-        None,
-    )
-    .await
-    .unwrap();
-    insert_history(
-        &db,
-        HistoryAction::Issued.into(),
-        Uuid::parse_str(&credential_id).unwrap().into(),
-        HistoryEntityType::Credential.into(),
-        organisation.id.to_owned().into(),
-    )
-    .await
-    .unwrap();
-
-    let claim_schema: Vec<(Uuid, &str, bool, u32, &str)> =
-        vec![(Uuid::new_v4(), "test", false, 0, "STRING")];
-    insert_many_claims_schema_to_database(&db, &credential_schema_id.to_string(), &claim_schema)
-        .await
-        .unwrap();
-
-    let claims: Vec<(Uuid, Uuid, &str, Vec<u8>)> = vec![(
-        Uuid::new_v4(),
-        claim_schema[0].0.to_owned(),
-        &credential_id,
-        vec![],
-    )];
-    insert_many_claims_to_database(&db, claims.as_slice())
-        .await
-        .unwrap();
-
-    let proof_schema_id = Uuid::parse_str(
-        &insert_proof_schema_with_claims_to_database(
-            &db,
-            None,
-            &claim_schema,
-            &organisation.id.to_string(),
-            "proof schema",
-        )
-        .await
-        .unwrap(),
-    )
-    .unwrap();
-    insert_history(
-        &db,
-        HistoryAction::Created.into(),
-        proof_schema_id.into(),
-        HistoryEntityType::ProofSchema.into(),
-        organisation.id.to_owned().into(),
-    )
-    .await
-    .unwrap();
-
-    let proof_id = Uuid::parse_str(
-        &insert_proof_request_to_database(&db, did_id, None, &proof_schema_id.to_string(), None)
-            .await
-            .unwrap(),
-    )
-    .unwrap();
-    let proof_claim: Vec<(Uuid, Uuid)> = vec![(proof_id.to_owned(), claims[0].0.to_owned())];
-    insert_many_proof_claim_to_database(&db, proof_claim.as_slice())
-        .await
-        .unwrap();
-
-    insert_history(
-        &db,
-        HistoryAction::Created.into(),
-        proof_id.into(),
-        HistoryEntityType::Proof.into(),
-        organisation.id.to_owned().into(),
-    )
-    .await
-    .unwrap();
+    } = setup_with_credential_schema_and_proof().await;
 
     let result = provider
         .get_history_list(HistoryListQuery {
@@ -391,4 +418,78 @@ async fn test_get_history_list_joins_schema_credential_claim_and_proof() {
     assert_eq!(1, result.total_pages);
     assert_eq!(expected_count as u64, result.total_items);
     assert_eq!(expected_count as usize, result.values.len());
+}
+
+#[tokio::test]
+async fn test_get_history_list_entity_of_another_type_should_not_get_fetched() {
+    let TestSetupWithCredentialsSchemaAndProof {
+        provider,
+        organisation,
+        did_id,
+        credential_schema_id,
+        ..
+    } = setup_with_credential_schema_and_proof().await;
+
+    let should_be_empty = provider
+        .get_history_list(HistoryListQuery {
+            pagination: Some(ListPagination {
+                page: 0,
+                page_size: 999,
+            }),
+            sorting: None,
+            filtering: Some(ListFilterCondition::And(vec![
+                ListFilterCondition::Value(HistoryFilterValue::OrganisationId(organisation.id)),
+                ListFilterCondition::Value(HistoryFilterValue::DidId(
+                    Uuid::parse_str(&credential_schema_id).unwrap().into(),
+                )),
+            ])),
+        })
+        .await
+        .unwrap();
+
+    assert_eq!(0, should_be_empty.total_pages);
+    assert_eq!(0, should_be_empty.total_items);
+    assert_eq!(0, should_be_empty.values.len());
+
+    let should_be_empty = provider
+        .get_history_list(HistoryListQuery {
+            pagination: Some(ListPagination {
+                page: 0,
+                page_size: 999,
+            }),
+            sorting: None,
+            filtering: Some(ListFilterCondition::And(vec![
+                ListFilterCondition::Value(HistoryFilterValue::OrganisationId(organisation.id)),
+                ListFilterCondition::Value(HistoryFilterValue::CredentialId(
+                    did_id.to_owned().into(),
+                )),
+            ])),
+        })
+        .await
+        .unwrap();
+
+    assert_eq!(0, should_be_empty.total_pages);
+    assert_eq!(0, should_be_empty.total_items);
+    assert_eq!(0, should_be_empty.values.len());
+
+    let should_be_empty = provider
+        .get_history_list(HistoryListQuery {
+            pagination: Some(ListPagination {
+                page: 0,
+                page_size: 999,
+            }),
+            sorting: None,
+            filtering: Some(ListFilterCondition::And(vec![
+                ListFilterCondition::Value(HistoryFilterValue::OrganisationId(organisation.id)),
+                ListFilterCondition::Value(HistoryFilterValue::CredentialSchemaId(
+                    did_id.to_owned().into(),
+                )),
+            ])),
+        })
+        .await
+        .unwrap();
+
+    assert_eq!(0, should_be_empty.total_pages);
+    assert_eq!(0, should_be_empty.total_items);
+    assert_eq!(0, should_be_empty.values.len());
 }
