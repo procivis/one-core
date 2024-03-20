046baf36251b384a8c5aa24f87da72a2cbef3cc5
Developer <info@procivis.ch>
2024-03-20 11:08:53 +0100

    ONE-1903: adapt bindings credential list query

--- platforms/uniffi/src/functions/get_credentials.rs
+++ platforms/uniffi/src/functions/get_credentials.rs
@@ -1,9 +1,13 @@
 use crate::{
     error::BindingError, utils::into_id, CredentialListBindingDTO, CredentialListQueryBindingDTO,
-    OneCoreBinding,
+    CredentialListQueryExactColumnBindingEnum, OneCoreBinding,
 };
+use dto_mapper::convert_inner;
 use one_core::{
-    model::{list_filter::ListFilterValue, list_query::ListPagination},
+    model::{
+        list_filter::{ListFilterValue, StringMatch, StringMatchType},
+        list_query::{ListPagination, ListSorting},
+    },
     service::credential::dto::{CredentialFilterValue, GetCredentialQueryDTO},
 };
 
@@ -16,9 +20,27 @@ impl OneCoreBinding {
             let core = self.use_core().await?;
 
             let condition = {
+                let exact = query.exact.unwrap_or_default();
+                let get_string_match_type = |column| {
+                    if exact.contains(&column) {
+                        StringMatchType::Equals
+                    } else {
+                        StringMatchType::StartsWith
+                    }
+                };
+
                 let organisation =
                     CredentialFilterValue::OrganisationId(into_id(&query.organisation_id)?)
                         .condition();
+
+                let name = query.name.map(|name| {
+                    CredentialFilterValue::Name(StringMatch {
+                        r#match: get_string_match_type(
+                            CredentialListQueryExactColumnBindingEnum::Name,
+                        ),
+                        value: name,
+                    })
+                });
 
                 let role = query
                     .role
@@ -35,7 +57,13 @@ impl OneCoreBinding {
                     None => None,
                 };
 
-                organisation & role & ids
+                let states = query.status.map(|values| {
+                    CredentialFilterValue::State(
+                        values.into_iter().map(|status| status.into()).collect(),
+                    )
+                });
+
+                organisation & name & role & ids & states
             };
 
             Ok(core
@@ -45,7 +73,10 @@ impl OneCoreBinding {
                         page: query.page,
                         page_size: query.page_size,
                     }),
-                    sorting: None,
+                    sorting: query.sort.map(|column| ListSorting {
+                        column: column.into(),
+                        direction: convert_inner(query.sort_direction),
+                    }),
                     filtering: Some(condition),
                 })
                 .await?
