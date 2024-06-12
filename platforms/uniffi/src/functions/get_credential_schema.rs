8dc713a386bb5105008b2bdbae197871823ac1ab
Developer <info@procivis.ch>
2024-06-12 11:22:44 +0200

    ONE-2693: Expose get_credential_schema to uniffi

---
+++ platforms/uniffi/src/functions/get_credential_schema.rs
@@ -0,0 +1,20 @@
+use crate::{error::BindingError, utils::into_id, CredentialSchemaBindingDTO, OneCoreBinding};
+use shared_types::CredentialSchemaId;
+
+impl OneCoreBinding {
+    pub fn get_credential_schema(
+        &self,
+        credential_schema_id: String,
+    ) -> Result<CredentialSchemaBindingDTO, BindingError> {
+        let credential_schema_id: CredentialSchemaId = into_id(&credential_schema_id)?;
+
+        self.block_on(async {
+            let core = self.use_core().await?;
+            Ok(core
+                .credential_schema_service
+                .get_credential_schema(&credential_schema_id)
+                .await?
+                .into())
+        })
+    }
+}
