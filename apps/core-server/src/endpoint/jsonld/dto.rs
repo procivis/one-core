0be671a5789013c12c92478f2e0456c4d3cd0b20
Pavel Zarecky <zarecky@procivis.ch>
2024-07-18 11:53:55 +0200

    ONE-2920: fixed swagger for JSON-LD fetch endpoint

--- apps/core-server/src/endpoint/jsonld/dto.rs
+++ apps/core-server/src/endpoint/jsonld/dto.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 use utoipa::{IntoParams, ToSchema};
 
 #[derive(Debug, Serialize, Deserialize, IntoParams)]
+#[into_params(parameter_in = Query)]
 pub struct ResolveJsonLDContextQuery {
     pub url: String,
 }
