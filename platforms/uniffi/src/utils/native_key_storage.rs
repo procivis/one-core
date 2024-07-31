a678f7780101ed85bf81a958d4da454c02e5f002
Developer <info@procivis.ch>
2024-07-31 10:30:52 +0200

    ONE-2992: fix unsupported key storage error propagation

--- platforms/uniffi/src/utils/native_key_storage.rs
+++ platforms/uniffi/src/utils/native_key_storage.rs
@@ -8,16 +8,12 @@ pub struct NativeKeyStorageWrapper(pub Box<dyn crate::dto::NativeKeyStorage>);
 
 impl one_core::provider::key_storage::secure_element::NativeKeyStorage for NativeKeyStorageWrapper {
     fn generate_key(&self, key_alias: String) -> Result<StorageGeneratedKey, KeyStorageError> {
-        Ok(self
-            .0
-            .generate_key(key_alias)
-            .map_err(|e| KeyStorageError::Failed(e.to_string()))?
-            .into())
+        Ok(self.0.generate_key(key_alias)?.into())
     }
 
     fn sign(&self, key_reference: &[u8], message: &[u8]) -> Result<Vec<u8>, SignerError> {
         self.0
             .sign(key_reference.into(), message.to_owned())
-            .map_err(|e| SignerError::CouldNotSign(e.to_string()))
+            .map_err(SignerError::from)
     }
 }
