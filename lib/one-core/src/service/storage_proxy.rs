ba5ae973ce3ed5eabbc54499707be193f51670c6
Developer <info@procivis.ch>
2024-10-07 15:41:56 +0200

    ONE-3077: not share deleted credential

--- lib/one-core/src/service/storage_proxy.rs
+++ lib/one-core/src/service/storage_proxy.rs
@@ -1,7 +1,6 @@
 use std::sync::Arc;
 
 use anyhow::Context;
-use one_dto_mapper::{convert_inner, convert_inner_of_inner};
 use shared_types::{CredentialSchemaId, DidId, DidValue, OrganisationId};
 
 use crate::model::claim::ClaimRelations;
@@ -43,12 +42,10 @@ impl StorageProxyImpl {
 #[async_trait::async_trait]
 impl StorageProxy for StorageProxyImpl {
     async fn create_interaction(&self, interaction: Interaction) -> anyhow::Result<InteractionId> {
-        convert_inner(
-            self.interactions
-                .create_interaction(interaction)
-                .await
-                .context("Create interaction error"),
-        )
+        self.interactions
+            .create_interaction(interaction)
+            .await
+            .context("Create interaction error")
     }
 
     async fn get_schema(
@@ -57,75 +54,69 @@ impl StorageProxy for StorageProxyImpl {
         schema_type: &str,
         organisation_id: OrganisationId,
     ) -> anyhow::Result<Option<CredentialSchema>> {
-        convert_inner_of_inner(
-            self.credential_schemas
-                .get_by_schema_id_and_organisation(
-                    schema_id,
-                    CredentialSchemaType::from(schema_type.to_string()),
-                    organisation_id,
-                    &CredentialSchemaRelations {
-                        claim_schemas: Some(Default::default()),
-                        organisation: Some(Default::default()),
-                    },
-                )
-                .await
-                .context("Error while fetching credential schema"),
-        )
+        self.credential_schemas
+            .get_by_schema_id_and_organisation(
+                schema_id,
+                CredentialSchemaType::from(schema_type.to_string()),
+                organisation_id,
+                &CredentialSchemaRelations {
+                    claim_schemas: Some(Default::default()),
+                    organisation: Some(Default::default()),
+                },
+            )
+            .await
+            .context("Error while fetching credential schema")
     }
 
     async fn get_credentials_by_credential_schema_id(
         &self,
         schema_id: &str,
     ) -> anyhow::Result<Vec<Credential>> {
-        convert_inner_of_inner(
-            self.credentials
-                .get_credentials_by_credential_schema_id(
-                    schema_id.to_owned(),
-                    &CredentialRelations {
-                        state: Some(Default::default()),
-                        issuer_did: Some(Default::default()),
-                        claims: Some(ClaimRelations {
-                            schema: Some(Default::default()),
-                        }),
-                        schema: Some(CredentialSchemaRelations {
-                            claim_schemas: Some(Default::default()),
-                            organisation: Some(Default::default()),
-                        }),
-                        ..Default::default()
-                    },
-                )
-                .await
-                .context("Error while fetching credential by credential schema id"),
-        )
+        Ok(self
+            .credentials
+            .get_credentials_by_credential_schema_id(
+                schema_id.to_owned(),
+                &CredentialRelations {
+                    state: Some(Default::default()),
+                    issuer_did: Some(Default::default()),
+                    claims: Some(ClaimRelations {
+                        schema: Some(Default::default()),
+                    }),
+                    schema: Some(CredentialSchemaRelations {
+                        claim_schemas: Some(Default::default()),
+                        organisation: Some(Default::default()),
+                    }),
+                    ..Default::default()
+                },
+            )
+            .await
+            .context("Error while fetching credential by credential schema id")?
+            .into_iter()
+            .filter(|cred| cred.deleted_at.is_none())
+            .collect::<Vec<_>>())
     }
 
     async fn create_credential_schema(
         &self,
         schema: CredentialSchema,
     ) -> anyhow::Result<CredentialSchemaId> {
-        convert_inner(
-            self.credential_schemas
-                .create_credential_schema(schema)
-                .await
-                .context("Create credential schema error"),
-        )
+        self.credential_schemas
+            .create_credential_schema(schema)
+            .await
+            .context("Create credential schema error")
     }
 
     async fn create_did(&self, did: Did) -> anyhow::Result<DidId> {
-        convert_inner(
-            self.dids
-                .create_did(did)
-                .await
-                .context("Could not fetch did by value"),
-        )
+        self.dids
+            .create_did(did)
+            .await
+            .context("Could not fetch did by value")
     }
 
     async fn get_did_by_value(&self, value: &DidValue) -> anyhow::Result<Option<Did>> {
-        convert_inner_of_inner(
-            self.dids
-                .get_did_by_value(value, &Default::default())
-                .await
-                .context("Could not fetch did by value"),
-        )
+        self.dids
+            .get_did_by_value(value, &Default::default())
+            .await
+            .context("Could not fetch did by value")
     }
 }
