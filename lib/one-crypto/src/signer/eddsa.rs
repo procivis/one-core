4a616c4d04104550824ddc3832594b0ec19fee04
Pavel Zarecky <zarecky@procivis.ch>
2024-10-23 11:57:54 +0200

    ONE-3652: moving EDDSA crypto logic to one-crypto

--- lib/one-crypto/src/signer/eddsa.rs
+++ lib/one-crypto/src/signer/eddsa.rs
@@ -1,6 +1,66 @@
+use ed25519_compact::PublicKey;
+use zeroize::Zeroizing;
+
 use crate::{Signer, SignerError};
 
 pub struct EDDSASigner {}
+
+pub struct KeyPair {
+    pub public: Vec<u8>,
+    pub private: Zeroizing<Vec<u8>>,
+}
+
+impl EDDSASigner {
+    pub fn check_public_key(public_key: &[u8]) -> Result<Vec<u8>, SignerError> {
+        let key = PublicKey::from_slice(public_key)
+            .map_err(|e| SignerError::CouldNotExtractPublicKey(e.to_string()))?;
+        Ok(key.to_vec())
+    }
+
+    pub fn public_key_from_der(public_key_der: &[u8]) -> Result<Vec<u8>, SignerError> {
+        let pk = ed25519_compact::PublicKey::from_der(public_key_der)
+            .map_err(|e| SignerError::CouldNotExtractPublicKey(e.to_string()))?;
+        Ok(pk.to_vec())
+    }
+
+    pub fn generate_key_pair() -> KeyPair {
+        let key_pair = ed25519_compact::KeyPair::generate();
+
+        KeyPair {
+            public: key_pair.pk.to_vec(),
+            private: key_pair.sk.to_vec().into(),
+        }
+    }
+
+    pub fn parse_private_key(secret_key: &[u8]) -> Result<KeyPair, SignerError> {
+        let secret_key = ed25519_compact::SecretKey::from_slice(secret_key)
+            .map_err(|_| SignerError::CouldNotExtractKeyPair)?;
+        let public_key = secret_key.public_key();
+
+        Ok(KeyPair {
+            public: public_key.to_vec(),
+            private: secret_key.to_vec().into(),
+        })
+    }
+
+    pub fn public_key_into_x25519(public_key_eddsa: &[u8]) -> Result<Vec<u8>, SignerError> {
+        let key = ed25519_compact::PublicKey::from_slice(public_key_eddsa)
+            .map_err(|e| SignerError::CouldNotExtractPublicKey(e.to_string()))?;
+        let key = ed25519_compact::x25519::PublicKey::from_ed25519(&key)
+            .map_err(|e| SignerError::CouldNotExtractPublicKey(e.to_string()))?;
+        Ok(key.to_vec())
+    }
+
+    pub fn private_key_into_x25519(
+        private_key_eddsa: &[u8],
+    ) -> Result<Zeroizing<Vec<u8>>, SignerError> {
+        let key = ed25519_compact::SecretKey::from_slice(private_key_eddsa)
+            .map_err(|_| SignerError::CouldNotExtractKeyPair)?;
+        let key = ed25519_compact::x25519::SecretKey::from_ed25519(&key)
+            .map_err(|_| SignerError::CouldNotExtractKeyPair)?;
+        Ok(key.to_vec().into())
+    }
+}
 
 impl Signer for EDDSASigner {
     fn sign(
