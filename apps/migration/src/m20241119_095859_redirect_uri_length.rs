0b652a84ea8cdd93c54ac669f2ffb8358e0445f9
Developer <info@procivis.ch>
2024-11-19 11:01:07 +0100

    ONE-2712: Bugfix

---
+++ apps/migration/src/m20241119_095859_redirect_uri_length.rs
@@ -0,0 +1,45 @@
+use sea_orm::DbBackend;
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        match manager.get_database_backend() {
+            DbBackend::MySql | DbBackend::Postgres => {
+                manager
+                    .alter_table(
+                        Table::alter()
+                            .table(Credential::Table)
+                            .modify_column(ColumnDef::new(Credential::RedirectUri).string_len(1000))
+                            .to_owned(),
+                    )
+                    .await?;
+                manager
+                    .alter_table(
+                        Table::alter()
+                            .table(Proof::Table)
+                            .modify_column(ColumnDef::new(Proof::RedirectUri).string_len(1000))
+                            .to_owned(),
+                    )
+                    .await?;
+            }
+            _ => {}
+        }
+        Ok(())
+    }
+}
+
+#[derive(Iden)]
+pub enum Credential {
+    Table,
+    RedirectUri,
+}
+
+#[derive(Iden)]
+pub enum Proof {
+    Table,
+    RedirectUri,
+}
