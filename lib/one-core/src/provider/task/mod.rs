4361f8e0aae34533d3952d810ce695c9ec189fe7
Developer <info@procivis.ch>
2025-05-23 11:58:07 +0200

    ONE-6001: fix suspend check

--- lib/one-core/src/provider/task/mod.rs
+++ lib/one-core/src/provider/task/mod.rs
@@ -7,13 +7,8 @@ use serde::de::DeserializeOwned;
 use serde_json::Value;
 
 use self::suspend_check::SuspendCheckProvider;
-use super::credential_formatter::provider::CredentialFormatterProvider;
-use super::did_method::provider::DidMethodProvider;
-use super::key_algorithm::provider::KeyAlgorithmProvider;
 use crate::config::core_config::{Fields, TaskConfig, TaskType};
 use crate::config::{ConfigError, ConfigParsingError};
-use crate::provider::key_storage::provider::KeyProvider;
-use crate::provider::revocation::provider::RevocationMethodProvider;
 use crate::provider::task::holder_check_credential_status::HolderCheckCredentialStatus;
 use crate::repository::certificate_repository::CertificateRepository;
 use crate::repository::claim_repository::ClaimRepository;
@@ -21,8 +16,6 @@ use crate::repository::credential_repository::CredentialRepository;
 use crate::repository::history_repository::HistoryRepository;
 use crate::repository::identifier_repository::IdentifierRepository;
 use crate::repository::proof_repository::ProofRepository;
-use crate::repository::revocation_list_repository::RevocationListRepository;
-use crate::repository::validity_credential_repository::ValidityCredentialRepository;
 use crate::service::certificate::CertificateService;
 use crate::service::credential::CredentialService;
 use crate::service::error::ServiceError;
@@ -45,17 +38,9 @@ pub(crate) fn tasks_from_config(
     claim_repository: Arc<dyn ClaimRepository>,
     credential_repository: Arc<dyn CredentialRepository>,
     history_repository: Arc<dyn HistoryRepository>,
-    revocation_method_provider: Arc<dyn RevocationMethodProvider>,
-    revocation_list_repository: Arc<dyn RevocationListRepository>,
-    validity_credential_repository: Arc<dyn ValidityCredentialRepository>,
-    formatter_provider: Arc<dyn CredentialFormatterProvider>,
-    did_method_provider: Arc<dyn DidMethodProvider>,
-    key_provider: Arc<dyn KeyProvider>,
-    key_algorithm_provider: Arc<dyn KeyAlgorithmProvider>,
     proof_repository: Arc<dyn ProofRepository>,
     certificate_repository: Arc<dyn CertificateRepository>,
     identifier_repository: Arc<dyn IdentifierRepository>,
-    core_base_url: Option<String>,
     credential_service: CredentialService,
     certificate_service: CertificateService,
 ) -> Result<HashMap<String, Arc<dyn Task>>, ConfigError> {
@@ -68,15 +53,8 @@ pub(crate) fn tasks_from_config(
 
         let provider = match &field.r#type {
             TaskType::SuspendCheck => Arc::new(SuspendCheckProvider::new(
-                credential_repository.to_owned(),
-                revocation_method_provider.to_owned(),
-                revocation_list_repository.to_owned(),
-                validity_credential_repository.to_owned(),
-                formatter_provider.to_owned(),
-                did_method_provider.to_owned(),
-                key_provider.to_owned(),
-                key_algorithm_provider.to_owned(),
-                core_base_url.to_owned(),
+                credential_repository.clone(),
+                credential_service.clone(),
             )) as _,
             TaskType::RetainProofCheck => Arc::new(RetainProofCheck::new(
                 claim_repository.clone(),
--- lib/one-core/src/provider/task/suspend_check/mod.rs
+++ lib/one-core/src/provider/task/suspend_check/mod.rs
@@ -5,67 +5,28 @@ use time::OffsetDateTime;
 
 use self::dto::SuspendCheckResultDTO;
 use super::Task;
-use crate::model::credential::{
-    Clearable, CredentialRelations, CredentialStateEnum, GetCredentialQuery,
-    UpdateCredentialRequest,
-};
-use crate::model::credential_schema::CredentialSchemaRelations;
-use crate::model::did::{DidRelations, KeyRole};
-use crate::model::identifier::IdentifierRelations;
-use crate::model::key::KeyRelations;
+use crate::model::credential::{CredentialStateEnum, GetCredentialQuery};
 use crate::model::list_filter::{ComparisonType, ListFilterValue, ValueComparison};
-use crate::model::organisation::OrganisationRelations;
-use crate::provider::credential_formatter::provider::CredentialFormatterProvider;
-use crate::provider::did_method::provider::DidMethodProvider;
-use crate::provider::key_algorithm::provider::KeyAlgorithmProvider;
-use crate::provider::key_storage::provider::KeyProvider;
-use crate::provider::revocation::error::RevocationError;
-use crate::provider::revocation::model::CredentialRevocationState;
-use crate::provider::revocation::provider::RevocationMethodProvider;
 use crate::repository::credential_repository::CredentialRepository;
-use crate::repository::revocation_list_repository::RevocationListRepository;
-use crate::repository::validity_credential_repository::ValidityCredentialRepository;
-use crate::service::credential::dto::CredentialFilterValue;
-use crate::service::error::{EntityNotFoundError, MissingProviderError, ServiceError};
-use crate::util::revocation_update::{generate_credential_additional_data, process_update};
+use crate::service::credential::CredentialService;
+use crate::service::credential::dto::{CredentialFilterValue, CredentialRole};
+use crate::service::error::ServiceError;
 
 pub mod dto;
 
 pub(crate) struct SuspendCheckProvider {
     credential_repository: Arc<dyn CredentialRepository>,
-    revocation_method_provider: Arc<dyn RevocationMethodProvider>,
-    revocation_list_repository: Arc<dyn RevocationListRepository>,
-    validity_credential_repository: Arc<dyn ValidityCredentialRepository>,
-    formatter_provider: Arc<dyn CredentialFormatterProvider>,
-    did_method_provider: Arc<dyn DidMethodProvider>,
-    key_provider: Arc<dyn KeyProvider>,
-    key_algorithm_provider: Arc<dyn KeyAlgorithmProvider>,
-    core_base_url: Option<String>,
+    credential_service: CredentialService,
 }
 
 impl SuspendCheckProvider {
-    #[allow(clippy::too_many_arguments)]
     pub fn new(
         credential_repository: Arc<dyn CredentialRepository>,
-        revocation_method_provider: Arc<dyn RevocationMethodProvider>,
-        revocation_list_repository: Arc<dyn RevocationListRepository>,
-        validity_credential_repository: Arc<dyn ValidityCredentialRepository>,
-        formatter_provider: Arc<dyn CredentialFormatterProvider>,
-        did_method_provider: Arc<dyn DidMethodProvider>,
-        key_provider: Arc<dyn KeyProvider>,
-        key_algorithm_provider: Arc<dyn KeyAlgorithmProvider>,
-        core_base_url: Option<String>,
+        credential_service: CredentialService,
     ) -> Self {
         SuspendCheckProvider {
             credential_repository,
-            revocation_method_provider,
-            revocation_list_repository,
-            validity_credential_repository,
-            formatter_provider,
-            did_method_provider,
-            key_provider,
-            key_algorithm_provider,
-            core_base_url,
+            credential_service,
         }
     }
 }
@@ -73,15 +34,15 @@ impl SuspendCheckProvider {
 #[async_trait::async_trait]
 impl Task for SuspendCheckProvider {
     async fn run(&self) -> Result<Value, ServiceError> {
-        let now: OffsetDateTime = OffsetDateTime::now_utc();
         let credential_list = self
             .credential_repository
             .get_credential_list(GetCredentialQuery {
                 filtering: Some(
                     CredentialFilterValue::State(vec![CredentialStateEnum::Suspended]).condition()
+                        & CredentialFilterValue::Role(CredentialRole::Issuer)
                         & CredentialFilterValue::SuspendEndDate(ValueComparison {
                             comparison: ComparisonType::LessThan,
-                            value: now,
+                            value: OffsetDateTime::now_utc(),
                         }),
                 ),
                 ..Default::default()
@@ -90,105 +51,9 @@ impl Task for SuspendCheckProvider {
 
         let credentials = credential_list.values;
 
-        for credential in credentials.iter() {
-            let credential_id = credential.id;
-            let credential = self
-                .credential_repository
-                .get_credential(
-                    &credential_id,
-                    &CredentialRelations {
-                        issuer_identifier: Some(IdentifierRelations {
-                            did: Some(DidRelations {
-                                keys: Some(KeyRelations::default()),
-                                ..Default::default()
-                            }),
-                            ..Default::default()
-                        }),
-                        holder_identifier: Some(IdentifierRelations {
-                            did: Some(Default::default()),
-                            ..Default::default()
-                        }),
-                        key: Some(KeyRelations::default()),
-                        schema: Some(CredentialSchemaRelations {
-                            organisation: Some(OrganisationRelations::default()),
-                            ..Default::default()
-                        }),
-                        ..Default::default()
-                    },
-                )
-                .await?;
-
-            let Some(credential) = credential else {
-                return Err(EntityNotFoundError::Credential(credential_id).into());
-            };
-
-            let Some(ref credential_schema) = credential.schema else {
-                return Err(ServiceError::MappingError(
-                    "credential_schema is None".to_string(),
-                ));
-            };
-
-            let revocation_method = self
-                .revocation_method_provider
-                .get_revocation_method(credential_schema.revocation_method.as_str())
-                .ok_or(MissingProviderError::RevocationMethod(
-                    credential_schema.revocation_method.to_string(),
-                ))?;
-
-            let issuer = credential
-                .issuer_identifier
-                .as_ref()
-                .ok_or(ServiceError::MappingError(
-                    "issuer_identifier is None".to_string(),
-                ))?
-                .did
-                .as_ref()
-                .ok_or(ServiceError::MappingError("issuer_did is None".to_string()))?;
-
-            let did_document = self.did_method_provider.resolve(&issuer.did).await?;
-
-            let Some(verification_method) =
-                did_document.find_verification_method(None, Some(KeyRole::AssertionMethod))
-            else {
-                return Err(ServiceError::Revocation(
-                    RevocationError::KeyWithRoleNotFound(KeyRole::AssertionMethod),
-                ));
-            };
-
-            let update = revocation_method
-                .mark_credential_as(
-                    &credential,
-                    CredentialRevocationState::Valid,
-                    generate_credential_additional_data(
-                        &credential,
-                        &*self.credential_repository,
-                        &*self.revocation_list_repository,
-                        &*revocation_method,
-                        &*self.formatter_provider,
-                        &self.key_provider,
-                        &self.key_algorithm_provider,
-                        &self.core_base_url,
-                        verification_method.id.to_owned(),
-                    )
-                    .await?,
-                )
-                .await?;
-            process_update(
-                update,
-                &*self.validity_credential_repository,
-                &*self.revocation_list_repository,
-            )
-            .await?;
-
-            self.credential_repository
-                .update_credential(
-                    credential_id,
-                    UpdateCredentialRequest {
-                        state: Some(CredentialStateEnum::Accepted),
-                        suspend_end_date: Clearable::ForceSet(None),
-                        ..Default::default()
-                    },
-                )
+        for credential in &credentials {
+            self.credential_service
+                .reactivate_credential(&credential.id)
                 .await?;
         }
 
@@ -200,6 +65,3 @@ impl Task for SuspendCheckProvider {
         serde_json::to_value(result).map_err(|e| ServiceError::MappingError(e.to_string()))
     }
 }
-
-#[cfg(test)]
-mod test;
