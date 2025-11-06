3c248e29ce3251a5a8dc868cfd8f0c537c715b89
Developer <info@procivis.ch>
2025-11-06 15:04:56 +0800

    ONE-7681: added missing get binding

--- platforms/uniffi/src/binding/wallet_unit.rs
+++ platforms/uniffi/src/binding/wallet_unit.rs
@@ -1,13 +1,15 @@
-use one_core::model::wallet_unit::WalletProviderType;
+use one_core::model::wallet_unit::{WalletProviderType, WalletUnitStatus};
 use one_core::service::wallet_unit::dto::{
-    HolderRefreshWalletUnitRequestDTO, HolderRegisterWalletUnitRequestDTO, WalletProviderDTO,
+    HolderRefreshWalletUnitRequestDTO, HolderRegisterWalletUnitRequestDTO,
+    HolderWalletUnitResponseDTO, WalletProviderDTO,
 };
 use one_dto_mapper::{From, Into, TryInto};
 
 use crate::ServiceError;
 use crate::binding::OneCoreBinding;
+use crate::binding::identifier::KeyListItemResponseBindingDTO;
 use crate::error::BindingError;
-use crate::utils::into_id;
+use crate::utils::{TimestampFormat, into_id};
 
 #[uniffi::export(async_runtime = "tokio")]
 impl OneCoreBinding {
@@ -17,8 +19,10 @@ impl OneCoreBinding {
         request: HolderRegisterWalletUnitRequestBindingDTO,
     ) -> Result<String, BindingError> {
         let core = self.use_core().await?;
-        let result = request.try_into();
-        let response = core.wallet_unit_service.holder_register(result?).await?;
+        let response = core
+            .wallet_unit_service
+            .holder_register(request.try_into()?)
+            .await?;
         Ok(response.to_string())
     }
 
@@ -26,12 +30,24 @@ impl OneCoreBinding {
     pub async fn holder_wallet_unit_status(&self, id: String) -> Result<(), BindingError> {
         let core = self.use_core().await?;
 
-        let id = into_id(&id)?;
+        Ok(core
+            .wallet_unit_service
+            .holder_wallet_unit_status(into_id(&id)?)
+            .await?)
+    }
+
+    #[uniffi::method]
+    pub async fn holder_get_wallet_unit(
+        &self,
+        id: String,
+    ) -> Result<HolderWalletUnitResponseBindingDTO, BindingError> {
+        let core = self.use_core().await?;
 
         Ok(core
             .wallet_unit_service
-            .holder_wallet_unit_status(id)
-            .await?)
+            .get_wallet_unit_details(into_id(&id)?)
+            .await?
+            .into())
     }
 }
 
@@ -67,4 +83,31 @@ pub struct HolderRefreshWalletUnitRequestBindingDTO {
 struct WalletProviderBindingDTO {
     url: String,
     r#type: WalletProviderTypeBindingEnum,
+}
+
+#[derive(Clone, Debug, From, uniffi::Record)]
+#[from(HolderWalletUnitResponseDTO)]
+pub struct HolderWalletUnitResponseBindingDTO {
+    #[from(with_fn_ref = "ToString::to_string")]
+    pub id: String,
+    #[from(with_fn_ref = "TimestampFormat::format_timestamp")]
+    pub created_date: String,
+    #[from(with_fn_ref = "TimestampFormat::format_timestamp")]
+    pub last_modified: String,
+    #[from(with_fn_ref = "ToString::to_string")]
+    pub provider_wallet_unit_id: String,
+    pub wallet_provider_url: String,
+    pub wallet_provider_type: WalletProviderTypeBindingEnum,
+    pub wallet_provider_name: String,
+    pub status: WalletUnitStatusBindingEnum,
+    pub authentication_key: KeyListItemResponseBindingDTO,
+}
+
+#[derive(Clone, Debug, uniffi::Enum, From)]
+#[from(WalletUnitStatus)]
+pub enum WalletUnitStatusBindingEnum {
+    Pending,
+    Active,
+    Revoked,
+    Error,
 }
