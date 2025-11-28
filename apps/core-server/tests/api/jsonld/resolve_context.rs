56da76bc15ffb9344acf1e627365a29034e469a6
Developer <info@procivis.ch>
2025-11-28 16:06:05 +0100

    chore: Resolve cached JSON-LD context in test

--- apps/core-server/tests/api/jsonld/resolve_context.rs
+++ apps/core-server/tests/api/jsonld/resolve_context.rs
@@ -9,7 +9,7 @@ async fn test_resolve_json_ld_context() {
     let resp = context
         .api
         .jsonld
-        .resolve("https://www.w3.org/2018/credentials/v1")
+        .resolve("https://www.w3.org/ns/credentials/v2") // preloaded in cache, so no network request needed
         .await;
     assert_eq!(resp.status(), 200);
 
